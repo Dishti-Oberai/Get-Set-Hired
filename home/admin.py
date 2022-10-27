f8baed219f5c4a4a1d01bc60a6917f4c8851ee0d
Dishti-Oberai <dishtioberai@gmail.com>
2022-10-27 18:52:01 +0530

    fourth version

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Skill, Profile
+from .models import Skill, Tag, Certificate, Jobposting, Message, Feedback, UserProfile, CompanyProfile, Room
 
 admin.site.register(Skill)
-admin.site.register(Profile)
+admin.site.register(Tag)
+admin.site.register(Certificate)
+admin.site.register(Jobposting)
+admin.site.register(Message)
+admin.site.register(Feedback)
+
+admin.site.register(UserProfile)
+admin.site.register(CompanyProfile)
+admin.site.register(Room)
